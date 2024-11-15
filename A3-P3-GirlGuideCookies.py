1284aeeb121fc70ae762a43a2746df77f397f438
Valentine Byrnes <W0516070@campus.nscc.ca>
2024-11-15 10:12:48 -0400

    A3P3 Girl Guide Cookies - Valentine Byrnes

--- A3-P3-GirlGuideCookies.py
+++ A3-P3-GirlGuideCookies.py
@@ -4,19 +4,49 @@
 #               those guides who go above and beyond in their sales efforts. The organizers
 #               want a program to print the guide list and their prizes.
 
-#Student #:     
-#Student Name:  
+#Student #: W0516070    
+#Student Name: Valentine Byrnes 
+
+
+    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+def guideInput():
+    return int(input("Enter the number of guides selling cookies: "))
+
+def guidesLoop(guideCount):
+    guideNames = []        
+    boxCount = []
+    for i in range(guideCount):
+        name = input(f"\nEnter the name of guide #{i + 1}: ")
+        guideNames.append(name)
+        boxes = int(input(f"Enter the number of boxes sold by {guideNames[i]}: "))
+        boxCount.append(boxes)
+    return guideNames, boxCount
+
+def avg(boxCount, guideCount):
+    totalBoxes = sum(boxCount)
+    avgBoxes = totalBoxes / guideCount
+    print(f"\n\nThe average number of boxes sold by each guide is: {avgBoxes:.1f}")
+    return avgBoxes
+
+def winner(guideNames, boxCount, avgBoxes):
+    print("\nGuide\t\t Prizes Won:")
+    print("---------------------------------------------------------------------------------")
+    for i in range(len(guideNames)):
+        prize = ["Trip to Girl Guide Jamboree in Aruba!", "Super Seller Badge", "Left over cookies", ""]
+        if boxCount[i] == max(boxCount):
+            print(f"{guideNames[i]:<16} - {prize[0]}")
+        elif boxCount[i] >= int(avgBoxes):
+            print(f"{guideNames[i]:<16} - {prize[1]}")
+        elif boxCount[i] < avgBoxes and boxCount[i] != 0:
+            print(f"{guideNames[i]:<16} - {prize[2]}")
+        else:
+            print(f"{guideNames[i]:<16} - {prize[3]}")
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
-
-
+    guideCount = guideInput()
+    guideNames, boxCounts = guidesLoop(guideCount)
+    avgBoxes = avg(boxCounts, guideCount)
+    winner(guideNames, boxCounts, avgBoxes)
     # YOUR CODE ENDS HERE
 
 main()
